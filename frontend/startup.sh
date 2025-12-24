1e499277d4ed9984d6ce6548ad23f1bb79734346
meroniohk <meron.mekonnen@iohk.io>
2025-12-25 02:10:45 +0300

    Change startup.sh

--- frontend/startup.sh
+++ frontend/startup.sh
@@ -47,15 +47,21 @@ else
 fi
 
 SPA_DEFAULT_LOCALE=${SPA_DEFAULT_LOCALE:-en_GB}
+SPA_PAGE_TITLE=${SPA_PAGE_TITLE:-"Taifa Care - KenyaEMR"}
 
 # Substitute environment variables in the html file
 # This allows us to override parts of the compiled file at runtime
 if [ -f "/usr/share/nginx/html/index.html" ]; then
-  envsubst '${IMPORTMAP_URL} ${SPA_PATH} ${API_URL} ${SPA_CONFIG_URLS} ${SPA_DEFAULT_LOCALE}' < "/usr/share/nginx/html/index.html" | sponge "/usr/share/nginx/html/index.html"
+  envsubst '${IMPORTMAP_URL} ${SPA_PATH} ${API_URL} ${SPA_CONFIG_URLS} ${SPA_DEFAULT_LOCALE} ${SPA_PAGE_TITLE}' < "/usr/share/nginx/html/index.html" | sponge "/usr/share/nginx/html/index.html"
 fi
 
 if [ -f "/usr/share/nginx/html/service-worker.js" ]; then
   envsubst '${IMPORTMAP_URL} ${SPA_PATH} ${API_URL}' < "/usr/share/nginx/html/service-worker.js" | sponge "/usr/share/nginx/html/service-worker.js"
+fi
+
+# Copy favicon.ico from assets to the html folder if it exists
+if [ -f "/usr/share/nginx/html/config/assets/kenyahmis-package/favicon.ico" ]; then
+  cp /usr/share/nginx/html/config/assets/kenyahmis-package/favicon.ico /usr/share/nginx/html/
 fi
 
 exec nginx -g "daemon off;"
