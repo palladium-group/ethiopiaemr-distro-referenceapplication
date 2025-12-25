dcda48cc82f03845021962d4c57af3a6beea31d8
meroniohk <meron.mekonnen@iohk.io>
2025-12-25 14:48:43 +0300

    Change nginx, startup

--- frontend/startup.sh
+++ frontend/startup.sh
@@ -1,67 +1,45 @@
 #!/bin/sh
 set -e
 
-# if we are using the $IMPORTMAP_URL environment variable, we have to make this useful,
-# so we change "importmap.json" into "$IMPORTMAP_URL" allowing it to be changed by envsubst
-if [ -n "${IMPORTMAP_URL}" ]; then
-  if [ -n "$SPA_PATH" ]; then
-    [ -f "/usr/share/nginx/html/index.html"  ] && \
-      sed -i -e 's/\("|''\)$SPA_PATH\/importmap.json\("|''\)/\1$IMPORTMAP_URL\1/g' "/usr/share/nginx/html/index.html"
+# Set default values
+SPA_DEFAULT_LOCALE="${SPA_DEFAULT_LOCALE:-am}"
+SPA_PATH="${SPA_PATH:-/openmrs/spa}"
 
-    [ -f "/usr/share/nginx/html/service-worker.js" ] && \
-      sed -i -e 's/\("|''\)$SPA_PATH\/importmap.json\("|''\)/\1$IMPORTMAP_URL\1/g' "/usr/share/nginx/html/service-worker.js"
-  else
-    [ -f "/usr/share/nginx/html/index.html"  ] && \
-      sed -i -e 's/\("|''\)importmap.json\("|''\)/\1$IMPORTMAP_URL\1/g' "/usr/share/nginx/html/index.html"
+# Convert SPA_CONFIG_URLS (comma-separated) to JSON array
+CONFIG_LIST=""
+OLD_IFS="$IFS"
+IFS=','
+for url in $SPA_CONFIG_URLS; do
+  CONFIG_LIST="${CONFIG_LIST}\"${url}\","
+done
+IFS="$OLD_IFS"
 
-    [ -f "/usr/share/nginx/html/service-worker.js" ] && \
-      sed -i -e 's/\("|''\)importmap.json\("|''\)/\1$IMPORTMAP_URL\1/g' "/usr/share/nginx/html/service-worker.js"
+# Remove trailing comma and wrap in brackets
+SPA_CONFIG_URLS_JSON="[${CONFIG_LIST%,}]"
+
+# Export variables for envsubst
+export IMPORTMAP_URL SPA_PATH API_URL SPA_DEFAULT_LOCALE SPA_CONFIG_URLS_JSON
+
+# Log values for debugging
+echo "DEBUG:"
+echo "  SPA_PATH=${SPA_PATH}"
+echo "  API_URL=${API_URL}"
+echo "  IMPORTMAP_URL=${IMPORTMAP_URL}"
+echo "  SPA_CONFIG_URLS=${SPA_CONFIG_URLS_JSON}"
+echo "  SPA_DEFAULT_LOCALE=${SPA_DEFAULT_LOCALE}"
+
+# Replace placeholders in index.html and service-worker.js
+for file in /usr/share/nginx/html/openmrs/spa/index.html /usr/share/nginx/html/openmrs/spa/service-worker.js; do
+  if [ -f "$file" ]; then
+    envsubst '${IMPORTMAP_URL} ${SPA_PATH} ${API_URL} ${SPA_CONFIG_URLS_JSON} ${SPA_DEFAULT_LOCALE}' < "$file" | sponge "$file"
   fi
+done
+
+# Copy favicon if it exists
+FAVICON="/usr/share/nginx/html/openmrs/spa/config/assets/ethiopiaemr-package/favicon.ico"
+if [ -f "$FAVICON" ]; then
+  cp "$FAVICON" /usr/share/nginx/html/openmrs/spa/
 fi
 
-# setting the config urls to "" causes an error reported in the console, so if we aren't using
-# the SPA_CONFIG_URLS, we remove it from the source, leaving config urls as []
-if [ -z "$SPA_CONFIG_URLS" ]; then
-  sed -i -e 's/"$SPA_CONFIG_URLS"//' "/usr/share/nginx/html/index.html"
-# otherwise convert the URLs into a Javascript list
-# we support two formats, a comma-separated list or a space separated list
-else
-  old_IFS="$IFS"
-  if echo "$SPA_CONFIG_URLS" | grep , >/dev/null; then
-    IFS=","
-  fi
-
-  CONFIG_URLS=
-  for url in $SPA_CONFIG_URLS;
-  do
-    if [ -z "$CONFIG_URLS" ]; then
-      CONFIG_URLS="\"${url}\""
-    else
-      CONFIG_URLS="$CONFIG_URLS,\"${url}\""
-    fi
-  done
-
-  IFS="$old_IFS"
-  export SPA_CONFIG_URLS=$CONFIG_URLS
-  sed -i -e 's/"$SPA_CONFIG_URLS"/$SPA_CONFIG_URLS/' "/usr/share/nginx/html/index.html"
-fi
-
-SPA_DEFAULT_LOCALE=${SPA_DEFAULT_LOCALE:-en_GB}
-SPA_PAGE_TITLE=${SPA_PAGE_TITLE:-"Taifa Care - KenyaEMR"}
-
-# Substitute environment variables in the html file
-# This allows us to override parts of the compiled file at runtime
-if [ -f "/usr/share/nginx/html/index.html" ]; then
-  envsubst '${IMPORTMAP_URL} ${SPA_PATH} ${API_URL} ${SPA_CONFIG_URLS} ${SPA_DEFAULT_LOCALE} ${SPA_PAGE_TITLE}' < "/usr/share/nginx/html/index.html" | sponge "/usr/share/nginx/html/index.html"
-fi
-
-if [ -f "/usr/share/nginx/html/service-worker.js" ]; then
-  envsubst '${IMPORTMAP_URL} ${SPA_PATH} ${API_URL}' < "/usr/share/nginx/html/service-worker.js" | sponge "/usr/share/nginx/html/service-worker.js"
-fi
-
-# Copy favicon.ico from assets to the html folder if it exists
-if [ -f "/usr/share/nginx/html/config/assets/kenyahmis-package/favicon.ico" ]; then
-  cp /usr/share/nginx/html/config/assets/kenyahmis-package/favicon.ico /usr/share/nginx/html/
-fi
-
+# Start nginx
 exec nginx -g "daemon off;"
